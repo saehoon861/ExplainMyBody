db02b26995fce14dca8cb8ef2de263e6537d671b
surya2347 <da0ne4338@gmail.com>
2026-01-29 09:45:49 +0900

    fix: 파일 의존성 에러 수정

--- backend/repositories/__init__.py
+++ backend/repositories/__init__.py
@@ -2,10 +2,10 @@
 Repository 레이어
 """
 
-from .user_repository import UserRepository
-from .health_record_repository import HealthRecordRepository
-from .analysis_report_repository import AnalysisReportRepository
-from .user_goal_repository import UserGoalRepository
+from .common.user_repository import UserRepository
+from .common.health_record_repository import HealthRecordRepository
+from .llm.analysis_report_repository import AnalysisReportRepository
+from .llm.user_goal_repository import UserGoalRepository
 
 __all__ = [
     "UserRepository",
