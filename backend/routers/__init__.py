683c9b8c231519cae62419b6e2e5e0f251348dd2
surya2347 <da0ne4338@gmail.com>
2026-02-02 11:04:10 +0900

    test: test1

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -4,7 +4,7 @@ API 라우터
 
 from .common import auth_router, users_router
 from .ocr import health_records_router
-from .llm import analysis_router, goals_router, weekly_plans_router
+from .llm import analysis_router, goals_router #, weekly_plans_router
 from .chatbot import router as chatbot_router
 
 __all__ = [
@@ -13,6 +13,6 @@ __all__ = [
     "health_records_router",
     "analysis_router",
     "goals_router",
-    "weekly_plans_router",
+    # "weekly_plans_router",
     "chatbot_router"
 ]
