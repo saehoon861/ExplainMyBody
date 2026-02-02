e7a2f2c2b8da16811fe7d5794b37319ceada00dd
surya2347 <da0ne4338@gmail.com>
2026-02-02 11:19:00 +0900

    revert: 백엔드일부 리롤

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -5,7 +5,7 @@ API 라우터
 from .common import auth_router, users_router
 from .ocr import health_records_router
 from .llm import analysis_router, goals_router #, weekly_plans_router
-from .chatbot import router as chatbot_router
+# from .chatbot import router as chatbot_router
 
 __all__ = [
     "auth_router",
@@ -14,5 +14,5 @@ __all__ = [
     "analysis_router",
     "goals_router",
     # "weekly_plans_router",
-    "chatbot_router"
+    # "chatbot_router"
 ]
